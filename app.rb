774bc71a2b4d321aaf1c7f35d3825f6b1bf0f492
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-11 19:25:24 +0500

    logger extracted to separate file

--- app.rb
+++ app.rb
@@ -1,32 +1,4 @@
-class Logger
+require './logger.rb'
 
-  def initialize
-    @f = File.open 'log.txt', 'a'
-  end
 
-  @@x = nil
-
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-
-    return @@x
-  end
-
-  def self.say_something
-    puts 'haha'
-  end
-
-  def log_something what
-    @f.puts what
-  end
-end
-
-Logger.say_something
-Logger.instance.log_something 'blabla'
-Logger.instance.log_something 'blabla'
-Logger.instance.log_something 'blabla'
-
-logger = Logger.new
-logger.log_something 'hey'
+Logger.instance.log_something 'blabla!!!'
