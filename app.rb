f141d65f47df6af1a2eb4dc52b7f9712c05f6e4a
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-11 18:41:21 +0500

    logger updated

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
 class Logger
+
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
   def self.say_something
     puts 'haha'
+  end
+
+  def log_something what
+    @f.puts what
   end
 end
 
 Logger.say_something
+
+logger = Logger.new
+logger.log_something 'hey'
