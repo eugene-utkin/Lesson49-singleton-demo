dd9bc14751e1de4818eb06d7f2230484b99314c4
Evgeniy_Utkin <eugene.utkin@gmail.com>
2021-04-09 18:24:20 +0500

    singleton demo from lesson 49

--- logger.rb
+++ logger.rb
@@ -1,18 +1,14 @@
+require 'singleton'
+
 class Logger
+  include Singleton
 
   def initialize
     @f = File.open 'log.txt', 'a'
-  end
-
-  @@x = Logger.new
-
-  def self.instance
-    return @@x
   end
 
   def log_something what
     @f.puts what
   end
 
-  private_class_method :new
 end
